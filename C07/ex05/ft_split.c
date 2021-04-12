be7cd1c05849008c45b4bf91207439070351f37d
Overraddit <kenwooshim@gmail.com>
2021-04-12 23:23:05 +0900

    C09

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// 단어가 0개면 널문자열 반환해야함.
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,6 +23,7 @@ int		ft_strlen(char *str)
 	while (str[i] != '\0')
 		i++;
 	return (i);
+
 }
 
 
@@ -47,40 +50,30 @@ int		ft_strstr(char *str, char *to_find)
 	return (-1);
 }
 
-int		sep_count(char *str, char *to_find)
+int		sep_count(char *str, char *charset)
 {
-	int i;
-	int j;
 	int count;
 
 	count = 0;
-	if (*to_find == '\0')
-		return (0);
-	i = 0;
-	while (str[i] != '\0')
+	while(*str)
 	{
-		j = 0;
-		while (to_find[j] != '\0')
-		{
-			if (str[i] == to_find[j])
-			{
-				count++;
-				break ;
-			}
-			j++;
-		}
-		i++;
+		if(check(*str, charset) == 1)
+			count++;
+		str++;
 	}
 	return (count);
 }
-/*
-	? charset은 하나의 구분자일까 아니면 각인덱스에 있는 각각의 문자들이 모두 하나의 구분자일까? 후자일듯...?
 
-	들어오는 배열?에 빈 문자열이 없으니 구분자의 개수 + 1 이 단어의 개수가 될 것이다.
-	1. 구분자를 기준으로 나눌때 단어의 개수는 몇개일까?
-	2. 단어의 개수 + 1 사이즈를 동적으로 할당받는다.
-	3. 짤라서 할당받은 메모리에 넣는다.
-*/
+int		check(char c, char *charset)
+{
+	while(*charset)
+	{
+		if(*charset == c)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
 
 char	*ft_strcpy(char *dest, char *src)
 {
@@ -89,7 +82,7 @@ char	*ft_strcpy(char *dest, char *src)
 
 	i = 0;
 	size = ft_strlen(src);
-	while (i < size - 1)
+	while (str[i] != '\0' || (str[i] != ))
 	{
 		dest[i] = src[i];
 		i++;
---
+++ C09/ex02/ft_split.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gshim <gshim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/12 22:03:42 by gshim             #+#    #+#             */
+/*   Updated: 2021/04/12 22:03:42 by gshim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		is_sep(char c, char *charset)
+{
+	while(*charset)
+	{
+		if(c == *charset)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
+
+int		get_word_count(char *str, char *charset)
+{
+	int ret;
+
+	ret = 0;
+	while(*str)
+	{
+		// 단어의 시작을 발견
+		if(!is_sep(*str, charset))
+		{
+			ret++;
+			// 단어가 끝나고 구분자를 찾을 때까지 전진
+			while(*str && !is_sep(*str, charset))
+				str++;
+		}
+	}
+	return (ret);
+}
+
+int		len(char *str, char *charset)
+{
+	int	len;
+
+	len = 0;
+	while(str[len] && !is_sep(str[len], charset))
+		len++;
+	return (len);
+}
+
+char **ft_split(char *str, char *charset)
+{
+	int		word_count;
+	int		i;
+	int		j;
+	char	**ret;
+
+	word_count = get_word_count(str, charset);
+	ret = (char **)malloc(sizeof(char*) * (word_count + 1));
+	i = 0;
+	while(i < word_count)
+	{
+		if(!is_sep(str, charset))
+		{
+			ret[i] = (char *)malloc(sizeof(char) * (len(str, charset) + 1));
+			j = 0;
+			while(*str && !is_sep(*str, charset))
+				ret[i][j++] = *(str++);
+			ret[i][j] = 0;
+			i++;
+		}
+		str++;
+	}
+	ret[i] = 0;
+	return (ret);
+}
